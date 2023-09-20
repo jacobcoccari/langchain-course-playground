389306ea57e5aec839e1123d57b358942d85fe82
jacobcoccari <jacob.coccari@gmail.com>
2023-09-19 20:40:57 -0600

    finally got the vectordb working but its still kind of weird

--- controller.py
+++ controller.py
@@ -13,6 +13,17 @@ import langchain
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
+import pickle
+import time
+
+# Our model
+from langchain.chat_models import ChatOpenAI
+
+# This retriever
+from langchain.retrievers import ContextualCompressionRetriever
+
+# This allows us to use
+from langchain.retrievers.document_compressors import LLMChainExtractor
 
 
 langchain.debug = True
@@ -20,19 +31,18 @@ langchain.debug = True
 
 class Controller:
     def __init__(self, openai_api_key, discord_api_key) -> None:
-        self.llm = ChatOpenAI(
-            model="gpt-4",
-            openai_api_key=openai_api_key,
-            temperature=0.0,
-        )
-        self.memory = ConversationBufferMemory(return_messages=True)
-        self.conversation = LLMChain(
-            llm=self.llm,
-            memory=self.memory,
-        )
-        # langchain.debug = True
         self.discord_api_key = discord_api_key
-        self.step = "start"
+        self.embedding_function = OpenAIEmbeddings()
+        self.llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+        self.compressor = LLMChainExtractor.from_llm(self.llm)
+        self.db_connection = Chroma(
+            persist_directory="./langchain_documents_db/",
+            embedding_function=self.embedding_function,
+        )
+        self.compression_retriever = ContextualCompressionRetriever(
+            base_compressor=self.compressor,
+            base_retriever=self.db_connection.as_retriever(),
+        )
 
     def query(self, query):
         prompt = PromptTemplate(
@@ -40,7 +50,9 @@ class Controller:
             input_variables=["query"],
         )
         _input = prompt.format_prompt(query=query).to_string()
-        return self.conversation.predict(human_input=_input)
+        # print(self.db_connection.similarity_search(_input)[0].page_content)
+        search = self.compression_retriever.get_relevant_documents(_input)
+        return search[0].page_content
 
     def run(self):
         discord_client = DiscordClient(self.query)
