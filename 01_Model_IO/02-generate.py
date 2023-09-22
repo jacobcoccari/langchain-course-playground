954774c4c088a3383ecf1f929082c50f17295a6d
jacobcoccari <jacob.coccari@gmail.com>
2023-09-22 15:31:18 -0600

    completed 02.02:

--- 01_Model_IO/02-generate.py
+++ 01_Model_IO/02-generate.py
@@ -8,10 +8,22 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 
 def main():
-    llm = OpenAI(openai_api_key=api_key, model="gpt-3.5-turbo-instruct")
-    result = llm.generate(["is this working?"]).dict()
-    print(pp.pp(result))
-    print(type(result))
+    llm = OpenAI(
+        openai_api_key=api_key,
+        model="gpt-3.5-turbo-instruct",
+    )
+    result = llm.generate(
+        [
+            "Write a limerick about taylor swift",
+            "tell me a joke",
+        ]
+    )
+    # This time, it returns an LLM result object as opposed to just a string
+    print(result)
+    # print(result.generations)
+    # print(type(result.generations))
+    # print(result.schema())
+    # print(result.llm_output)
 
 
 if __name__ == "__main__":
