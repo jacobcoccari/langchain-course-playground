98456e6863a2318f5384729107a4339a78fd347c
jacobcoccari <jacob.coccari@gmail.com>
2023-09-27 20:32:00 -0600

    changed and simplified sequentialchain

--- 03_Chains/02-sequential-chain.py
+++ 03_Chains/02-sequential-chain.py
@@ -1,66 +1,63 @@
 from dotenv import load_dotenv
 
-from langchain.chains import SequentialChain
-from langchain.chains import LLMChain
-from langchain.prompts import ChatPromptTemplate
-
-
 load_dotenv()
 
+from langchain.chains import SequentialChain, LLMChain
+from langchain.prompts import ChatPromptTemplate
 from langchain.chat_models import ChatOpenAI
 
 # This is an LLMChain to write a synopsis given a title of a play and the era it is set in.
 model = ChatOpenAI()
-synopsis_template = """You are a playwright. Given the title of play and the era it is set in, \
-                    it is your job to write a synopsis for that title. Be concise.
-
-Title: {title}
-Era: {era}
-Playwright: This is a synopsis for the above play:"""
-synopsis_prompt_template = ChatPromptTemplate.from_template(synopsis_template)
-# input_variables=["title", "era"], template=synopsis_template
-
-synopsis_chain = LLMChain(
-    llm=model,
-    prompt=synopsis_prompt_template,
-    output_key="synopsis",
+idea_template = (
+    """ You are a poet. Generate a single topic idea for a poem written by {person}"""
 )
 
-# This is an LLMChain to write a review of a play given a synopsis.
-review_template = """You are a play critic from the New York Times. Given the synopsis of play, it is \
-            your job to write a review for that play. Be concise.
+poem_template = """Write a {style} poem about {topic}."""
 
-Play Synopsis:
-{synopsis}
-Review from a New York Times play critic of the above play:"""
-review_prompt_template = ChatPromptTemplate.from_template(review_template)
-# input_variables=["synopsis"], template=template
-review_chain = LLMChain(
-    llm=model,
-    prompt=review_prompt_template,
-    output_key="review",
+feedback_template = """You are a poetry teacher teaching {age_level}. \ 
+                    Given the following poem, provide concise feedback to the student.
+                    Poem: {poem}"""
+
+idea_prompt_template = ChatPromptTemplate.from_template(idea_template)
+poem_prompt_template = ChatPromptTemplate.from_template(poem_template)
+feedback_prompt_template = ChatPromptTemplate.from_template(feedback_template)
+
+idea_chain, poem_chain, feedback_chain = (
+    LLMChain(
+        llm=model,
+        prompt=idea_prompt_template,
+        output_key="topic",
+    ),
+    LLMChain(
+        llm=model,
+        prompt=poem_prompt_template,
+        output_key="poem",
+    ),
+    LLMChain(
+        llm=model,
+        prompt=feedback_prompt_template,
+        output_key="feedback",
+    ),
 )
-
-# This is the overall chain where we run these two chains in sequence.
 
 overall_chain = SequentialChain(
-    chains=[synopsis_chain, review_chain],
-    input_variables=["era", "title"],
+    chains=[idea_chain, poem_chain, feedback_chain],
+    input_variables=["person", "style", "age_level"],
     # Here we return multiple variables
-    output_variables=["synopsis", "review"],
+    output_variables=["poem", "feedback"],
 )
 
 # .run is not supported for SequentialChain since there is more than one output key, .generate not supportede
 result = overall_chain(
-    {"title": "Tragedy at sunset on the beach", "era": "Victorian England"}""
+    {"person": "a pirate", "style": "haiku", "age_level": "9th graders"}
 )
 print(result)
 
 # .run is not supported for SequentialChain since there is more than one output key, .generate not supportede
 result = overall_chain.apply(
     [
-        {"title": "Tragedy at sunset on the beach", "era": "Victorian England"},
-        {"title": "Tragedy at sunset on the beach", "era": "Modern Broadway"},
+        {"person": "a pirate", "style": "haiku", "age_level": "9th graders"},
+        {"person": "a carpenter", "style": "sonnet", "age_level": "7th graders"},
     ]
 )
 print(result)
---
+++ 03_Chains/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+result = overall_chain.apply(
+#     [
+#         {"title": "Tragedy at sunset on the beach", "era": "Victorian England"},
+#         {"title": "Tragedy at sunset on the beach", "era": "Modern Broadway"},
+#     ]
+# )
+# print(result)
