ffc73782f0b45c5baa826260c154cbda2ec658b6
jacobcoccari <jacob.coccari@gmail.com>
2023-09-28 14:08:28 -0600

    adding message history

---
+++ 04_Memory/04-conversationbuffermemory-with-systemmessage.py
@@ -0,0 +1,7 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import SystemMessage
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+)
