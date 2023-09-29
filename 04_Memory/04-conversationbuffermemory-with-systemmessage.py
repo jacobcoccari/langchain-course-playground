53c0a929a25b7b572fea8b858bff8bf2a01b836d
jacobcoccari <jacob.coccari@gmail.com>
2023-09-29 11:39:26 -0600

    finally figured it out

--- 04_Memory/04-conversationbuffermemory-with-systemmessage.py
+++ 04_Memory/04-conversationbuffermemory-with-systemmessage.py
@@ -1,7 +1,35 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage
+from langchain.memory import ConversationBufferMemory
 from langchain.prompts import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
     MessagesPlaceholder,
 )
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        SystemMessage(
+            content="You are a chatbot having a conversation with a human."
+        ),  # The persistent system prompt
+        MessagesPlaceholder(
+            variable_name="chat_history"
+        ),  # Where the memory will be stored.
+        HumanMessagePromptTemplate.from_template(
+            "{human_input}"
+        ),  # Where the human input will injected
+    ]
+)
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+llm = ChatOpenAI()
+
+chat_llm_chain = LLMChain(
+    llm=llm,
+    prompt=prompt,
+    verbose=True,
+    memory=memory,
+)
+
+chat_llm_chain.predict(human_input="Hi there my friend")
