b73274eb72086d97959a5f34bcac1b101594f7d0
jacobcoccari <jacob.coccari@gmail.com>
2023-10-01 22:23:06 -0600

    completed the chatgpt clone.

--- 06-ChatGPT-Clone/main-without-streaming.py
+++ 06-ChatGPT-Clone/main-without-streaming.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 load_dotenv()
 model = ChatOpenAI()
