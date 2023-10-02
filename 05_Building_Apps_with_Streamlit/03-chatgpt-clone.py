0a1d903ec04445e2a5e78c85148ce1f71a920d0c
jacobcoccari <jacob.coccari@gmail.com>
2023-10-02 14:11:40 -0600

    added streamlit chatgpt clone lecture

--- 05_Building_Apps_with_Streamlit/03-chatgpt-clone.py
+++ 05_Building_Apps_with_Streamlit/03-chatgpt-clone.py
@@ -1,55 +1,41 @@
+import os
 import streamlit as st
 from dotenv import load_dotenv
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+import openai
 
 load_dotenv()
-model = ChatOpenAI()
-memory = ConversationBufferMemory(return_messages=True)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def generate_assistant_response(prompt):
-    conversation = ConversationChain(
-        llm=model,
-        memory=memory,
+def generate_assistant_response():
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=st.session_state.messages
     )
-    response = conversation.predict(input=prompt)
-    return response
-
-
-def save_chat_history(prompt, messages):
-    st.session_state.messages.append(
-        {
-            "role": "user",
-            "content": prompt,
-        }
-    )
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    assistant_response = generate_assistant_response(prompt)
-    with st.chat_message("assistant"):
-        st.markdown(assistant_response)
-    st.session_state.messages.append(
-        {
-            "role": "assistant",
-            "content": assistant_response,
-        }
-    )
+    return response.choices[0].message["content"]
 
 
 def main():
-    st.title("ChatGPT Clone")
+    st.title("ChatGPT-like clone")
+
     if "messages" not in st.session_state:
         st.session_state.messages = []
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
+
     prompt = st.chat_input("What is up?")
 
     if prompt:
-        save_chat_history(prompt, st.session_state.messages)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        assistant_response = generate_assistant_response()
+        with st.chat_message("assistant"):
+            st.markdown(assistant_response)
+
+        st.session_state.messages.append(
+            {"role": "assistant", "content": assistant_response}
+        )
 
 
 if __name__ == "__main__":
