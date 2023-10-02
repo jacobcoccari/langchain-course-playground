5e7bbfa094a76bcc69cb0c7cfed372574b7ab9b6
jacobcoccari <jacob.coccari@gmail.com>
2023-10-02 11:18:00 -0600

    adding more basic prompts

--- 00-Basic-Prompting-Techniques/01-text-insertion-and-extraction.py
+++ 00-Basic-Prompting-Techniques/01-text-insertion-and-extraction.py
@@ -14,5 +14,21 @@ def chat(message, model="gpt-3.5-turbo"):
     return response.choices[0].message["content"]
 
 
+# Text insertion
 result = chat("The [blank] refers to the rule by the father.")
 print(result)
+
+prompt = """I kayaked across the Hudson.
+
+Please identify the name of the river in this passage.
+"""
+
+response = chat(prompt)
+print(response)
+
+prompt = """Please identify the name of the river in the following passages:
+1. I kayaked across the hudson
+2. I swam across the Mississippi
+"""
+response = chat(prompt)
+print(response)
