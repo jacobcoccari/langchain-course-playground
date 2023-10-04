8f55eb1e710c4bb4e8df899328bb6f31675c2ffa
jacobcoccari <jacob.coccari@gmail.com>
2023-10-04 10:39:40 -0600

    self query retriever is acting wierd.... query function not working.

--- 10-Retriver-Augmented-Generation/03-self-querying-retriever.py
+++ 10-Retriver-Augmented-Generation/03-self-querying-retriever.py
@@ -3,6 +3,7 @@ from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.retrievers.self_query.base import SelfQueryRetriever
 from langchain.chains.query_constructor.base import AttributeInfo
 from langchain.llms import OpenAI
+from langchain.schema import Document
 from langchain.chains import RetrievalQA
 
 # pip install lark
@@ -11,60 +12,97 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+docs = [
+    Document(
+        page_content="A bunch of scientists bring back dinosaurs and mayhem breaks loose",
+        metadata={
+            "year": 1993,
+            "rating": 7.7,
+            "genre": "action",
+        },
+    ),
+    Document(
+        page_content="Leo DiCaprio gets lost in a dream within a dream within a dream within a ...",
+        metadata={"year": 2010, "director": "Christopher Nolan", "rating": 8.2},
+    ),
+    Document(
+        page_content="A psychologist / detective gets lost in a series of dreams within dreams within dreams and Inception reused the idea",
+        metadata={
+            "year": 2006,
+            "director": "Satoshi Kon",
+            "rating": 8.6,
+        },
+    ),
+    Document(
+        page_content="A bunch of normal-sized women are supremely wholesome and some men pine after them",
+        metadata={
+            "year": 2019,
+            "director": "Greta Gerwig",
+            "rating": 8.3,
+        },
+    ),
+    Document(
+        page_content="Toys come alive and have a blast doing so",
+        metadata={
+            "year": 1995,
+            "genre": "animated",
+        },
+    ),
+    Document(
+        page_content="Three men walk into the Zone, three men walk out of the Zone",
+        metadata={
+            "year": 1979,
+            "rating": 9.9,
+            "director": "Andrei Tarkovsky",
+            "genre": "science fiction",
+            "rating": 9.9,
+        },
+    ),
+]
 embedding_function = HuggingFaceInstructEmbeddings(
     model_name="hkunlp/instructor-base",
 )
 
-db = Chroma(
-    persist_directory="./10-Retriver-Augmented-Generation/crash-course-db",
-    embedding_function=embedding_function,
+db = Chroma.from_documents(
+    docs,
+    embedding_function,
+    persist_directory="./10-Retriver-Augmented-Generation/movies-db",
 )
+db.persist()
 # Take a look at the metadata
 # print(db.similarity_search("who was ashoka?"))
 
 metadata_field_info = [
     AttributeInfo(
-        name="author",
-        description="The author of the video transcript",
+        name="genre",
+        description="The genre of the movie",
         type="string or list[string]",
     ),
     AttributeInfo(
-        name="description",
-        description="the description of the video",
+        name="year",
+        description="The year the movie was released",
+        type="integer",
+    ),
+    AttributeInfo(
+        name="director",
+        description="The name of the movie director",
         type="string",
     ),
     AttributeInfo(
-        name="publish_date",
-        description="The date that the video was originally published",
-        type="datetime",
-    ),
-    AttributeInfo(name="source", description="The youtube URL slug.", type="string"),
-    AttributeInfo(
-        name="thumbnail_url",
-        description="The URL for the video thumbnail.",
-        type="string",
-    ),
-    AttributeInfo(
-        name="title",
-        description="the title of the video",
-        type="string",
-    ),
-    AttributeInfo(
-        name="view_count",
-        description="The total number of views for the video",
-        type="int",
+        name="rating", description="A 1-10 rating for the movie", type="float"
     ),
 ]
+
 model = OpenAI()
-description = "Metadata and transcripts from a youtube video."
+description = "Brief summary of a movie"
 retriever = SelfQueryRetriever.from_llm(
-    llm=model,
-    vectorstore=db,
-    document_contents=description,
-    metadata_field_info=metadata_field_info,
+    model,
+    db,
+    description,
+    metadata_field_info,
     verbose=True,
-    k=1,
 )
+
 
 query = "What video has the most total views?"
 response = retriever.get_relevant_documents(query)
